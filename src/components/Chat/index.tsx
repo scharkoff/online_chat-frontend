04e19d01df8af3722c5099280e52a4276aac919f
Sharkov Ivan <sharkov.agent@mail.ru>
2023-03-17 09:29:43 +0300

    Base adaptive for gadgets

--- src/components/Chat/index.tsx
+++ src/components/Chat/index.tsx
@@ -1,16 +1,16 @@
-import React from "react";
+import React from 'react';
 
-import Paper from "@mui/material/Paper";
-import UserList from "../UserList";
-import Messages from "../Messages";
+import Paper from '@mui/material/Paper';
+import UserList from '../UserList';
+import Messages from '../Messages';
 
-import styles from "./scss/Chat.module.scss";
-import AddMessage from "../AddMessage";
-import { IChatProps } from "./types";
+import styles from './scss/Chat.module.scss';
+import AddMessage from '../AddMessage';
+import { IChatProps } from './types';
 
 export default function Chat({ addMessage }: IChatProps) {
   return (
-    <Paper elevation={1} style={{ display: "flex" }}>
+    <Paper elevation={1} className={styles.chat}>
       <UserList />
       <div className={styles.chatWindow}>
         <Messages />
